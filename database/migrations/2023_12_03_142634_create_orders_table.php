f5f954a623079f28c49160671d333368b747f58c
Jahid Hasan <jahidhasanofficial23@gmail.com>
2023-12-03 20:36:48 +0600

    assignment add

---
+++ database/migrations/2023_12_03_142634_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity');
+            $table->timestamps();
+            // Foreign key relationship
+           
+            $table->foreign('product_id')->references('id')->on('products');
+           $table->timestamp('created_to')->useCurrent();
+           $table->timestamp('updated_to')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
